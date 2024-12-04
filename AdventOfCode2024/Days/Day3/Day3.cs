50ed83f1c2e48df9783d46b903ecda5364066058
cc121 <ccooper91@gatech.edu>
2024-12-03 17:34:46 -0700

    Day3 Part2

--- AdventOfCode2024/Days/Day3/Day3.cs
+++ AdventOfCode2024/Days/Day3/Day3.cs
@@ -22,7 +22,30 @@ namespace AdventOfCode2024.Days.Day3
 
         public override string Part2(string input)
         {
-            throw new NotImplementedException();
+            string pattern = @"mul\(([0-9]{1,3}),([0-9]{1,3})\)|don\'t\(\)|do\(\)";
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            int result = 0;
+            int factor = 1;
+            foreach (Match match in matches)
+            {
+                if (match.ToString().Contains("don't"))
+                {
+                    factor = 0;
+                }
+                else if (match.ToString().Contains("do"))
+                {
+                    factor = 1;
+                }
+                else
+                {
+                    int num1 = int.Parse(match.Groups[1].Value);
+                    int num2 = int.Parse(match.Groups[2].Value);
+                    result += factor * num1 * num2;
+                }
+            }
+
+            return result.ToString();
         }
     }
 }
